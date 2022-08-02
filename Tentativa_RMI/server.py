7685a6cf352246bcff411c51a54f763a37769687
Felipe Gabriel <felipegabriel.fraga@hotmail.com>
2022-08-01 22:18:47 -0300

    Update server.py

--- Tentativa_RMI/server.py
+++ Tentativa_RMI/server.py
@@ -1,9 +1,23 @@
 import Pyro4
 
-
 @Pyro4.expose
 class funcaoremota(object):
-    
+     def maioridade(self, sexo, idade):
+        idadeint =  int(idade)
+        if(sexo == "masculino"):
+            if(idadeint >= 18):
+                return "Maior de idade"
+            else:
+                return "Menor de idade"
+        else:
+            if(sexo == "feminino"):
+                if(idadeint >= 21):
+                    return "Maior de idade"
+                else:
+                    return "Menor de idade"
+            else:
+                return "Informação inconstante"
+
     daemon = Pyro4.Daemon()
     ns = Pyro4.locateNS()
     uri = daemon.register(funcaoremota)
@@ -11,21 +25,3 @@ class funcaoremota(object):
 
     print("Escutando...")
     daemon.requestLoop()
-
-    
-    def maioridade(self, sexo, idade):
-        idadeint =  int(idade)
-        if(sexo == "masculino"):
-            if(idadeint>=18):
-                return "Maior de idade"
-            else:
-                return "Menor de idade"
-        else:
-            if(sexo == "feminino"):
-                if(idadeint>=21):
-                    return "Maior de idade"
-                else:
-                    return "Menor de idade"
-            else:
-                return "Informação inconstante"
-
